993bf5cce0a84b95e25cad183c39dddf3f9136cd
scarletjara <scarlet.jara@hotmail.com>
2026-06-25 00:30:10 -0400

    actualizacion

--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,7 @@
 // Determinar la URL base de la API según el host
 // frontend/app.js
 
-const API_BASE = "http://10.0.138.17:3001/api/productos";
+const API_BASE = "http://localhost:3001/api/productos";
 
 // Ejemplo: const API_BASE = "http://localhost:3001/api/productos";
 
